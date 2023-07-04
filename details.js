2d695f20fbddd88ef3c72e883aa6e4242fc91500
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-07-04 19:37:18 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: '6381560615:AAHmSMZE7iDmI-iDmCnRIrntlL5p4fWlC74',
+    admins: [5556717138],
+    mongo_url: 'mongodb+srv://manishprajapati7517: R4gO0q3MFPDbjqKp@kavyatg.zgsh5nz.mongodb.net/?retryWrites=true&w=majority',
+}
+
